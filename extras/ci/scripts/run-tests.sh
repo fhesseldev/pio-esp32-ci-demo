d8c6fe82688ec959df94f12b0bcba3cc3f591ae3
Frank Hessel <frank@fhessel.de>
2020-03-03 03:01:30 +0100

    Fix escaping of wifi credentials

--- extras/ci/scripts/run-tests.sh
+++ extras/ci/scripts/run-tests.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Read hostapd config and add WIFI_PSK and WIFI_SSID defines to the build environment
-WIFI_SSID="$(grep ssid /etc/hostapd/hostapd.conf | sed -E 's/ssid=(.*)/\1/')"
+WIFI_SSID="$(grep -E '^ssid=' /etc/hostapd/hostapd.conf | sed -E 's/ssid=(.*)/\1/')"
 WIFI_PSK="$(grep wpa_passphrase /etc/hostapd/hostapd.conf | sed -E 's/wpa_passphrase=(.*)/\1/')"
 
 # Pass the wifi credentials to the Platform IO build process
-export PLATFORMIO_BUILD_FLAGS="-DWIFI_SSID=\"\\\"$WIFI_SSID\\\"\" -DWIFI_PSK=\"\\\"$WIFI_PSK\\\"\""
+export PLATFORMIO_BUILD_FLAGS="-DWIFI_SSID=\\\"$WIFI_SSID\\\" -DWIFI_PSK=\\\"$WIFI_PSK\\\""
 
 # Serial port on GPIO header of the Rasperry Pi
 export PORT="/dev/ttyS0"
